b62cb7d77597090d0172f59829ab6b2e0ffa077c
Manai Yassine <98694575+YassineManai@users.noreply.github.com>
2024-11-11 17:09:25 +0100

    finale changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/YassineManai">
           <div>Designed &amp; Built by Yassine Manai </div>
         </a>
       </StyledCredit>
